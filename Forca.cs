010db8a58352df1fee4042ca5a0df602c2f1aa97
Gabriel Batista <gabriel.dsbatista28@gmail.com>
2023-09-19 17:41:26 -0300

    implementacao dos metodos 'SorteaPalavra' 'Validacao' 'OrganizaPalvra' 'SalvaLetras' e alguns ajustes

--- Forca.cs
+++ Forca.cs
@@ -8,29 +8,27 @@ namespace JogoForca
 {
     internal class Forca
     {
-        public string Palavra { get; private set; }
         public int QtdErro { get; private set; }
+        public string Dica { get; private set; }
+        public string Palavra { get; private set; }
+        public char[] LayoutPalavra { get; private set; }
+        public char[] LetrasDig { get; private set; }
 
-        public char[] PalavraArray { get; private set; }
 
-        public Forca(string palavra, char[] palavraArray)
-        {
-            Palavra = palavra;
-            PalavraArray = palavraArray;
-        }
+        Random random = new Random();
 
-        public void VerificaLetra(char letra)
+        public void VerificaLetra(char letra, string palavra)
         {
             int i = 0;
             int acerto = 0;
-            foreach (char c in this.Palavra)
+            foreach (char c in palavra)
             {
                 if (c == letra)
                 {
-                    this.PalavraArray[i] = letra;
+                    this.LayoutPalavra[i] = letra;
                     acerto++;
                 }
-                if (i == this.Palavra.Length-1 && acerto == 0)
+                if (i == palavra.Length-1 && acerto == 0)
                 {
                
                     this.Erro();
@@ -80,7 +78,88 @@ namespace JogoForca
                     break;
             }
         }
-        
+
+        public void SorteaPalavra()
+        {
+            string[] dicas = new string[] { "Animal", "Personagem de Anime", "Super-Heroi" };
+
+            string[] palavrasAnimal = new string[] { "MACACO", "JACARE", "BALEIA", "URSO", "LEAO", "PATO" };
+            string[] palavrasPersonagem = new string[] { "NARUTO", "GOKU", "LUFFY", "EREN", "SAITAMA" };
+            string[] palavrasSupers = new string[] { "BATMAN", "SUPERMAN", "AQUAMAN" };
+
+            string palavra = "";
+            int qtd;
+            int dica = random.Next(0, 2);
+
+            this.Dica = dicas[dica];
+            if (dica == 0)
+            {
+                qtd = random.Next(0, palavrasAnimal.Length);
+                palavra = palavrasAnimal[qtd];
+            }
+            if (dica == 1)
+            {
+                qtd = random.Next(0, palavrasPersonagem.Length);
+                palavra = palavrasPersonagem[qtd];
+            }
+            if (dica == 2)
+            {
+                qtd = random.Next(0, palavrasSupers.Length);
+                palavra = palavrasSupers[qtd];
+            }
+
+            this.Palavra = palavra;
+        }
+
+        public void OrganizaPalavra(string palavra)
+        {
+            char[] palavraA = new char[palavra.Length];
+            for (int i = 0; i < palavraA.Length; i++)
+            {
+                palavraA[i] = '_';
+            }
+
+            this.LayoutPalavra = palavraA;
+        }
+
+        public bool Validacao()
+        {
+            int i = 0;
+
+            if (this.QtdErro == 6)
+            {
+                Console.Clear();
+                Console.WriteLine("Dica: " + this.Dica);
+                Console.WriteLine(string.Join(" ", this.LetrasDig));
+                this.Desenha();
+                Console.WriteLine("Que Pena. A palavra era: " + this.Palavra);
+                return true;
+            }
+            foreach (char c in this.LayoutPalavra)
+            {
+                if (c == '_')
+                    break;
+                if (i == this.LayoutPalavra.Length - 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Dica: " + this.Dica);
+                    Console.WriteLine(string.Join(" ", this.LetrasDig));
+                    this.Desenha();
+                    Console.WriteLine(string.Join(" ", this.LayoutPalavra));
+                    Console.WriteLine("Parabens!!!");
+                    return true;
+                }
+                i++;
+            }
+            return false;
+        }
+
+        public void SalvaLetras(char letra, int i)
+        {   
+            if (i == 0)
+                this.LetrasDig = new char[this.Palavra.Length + 6];
+            this.LetrasDig[i] = letra;
+        }
         public void Erro()
         {
             this.QtdErro++;
--- Program.cs
+++ Program.cs
@@ -4,42 +4,23 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Random random = new Random();
-
-        string[] dicas = new string[] { "Animal", "Personagem de Anime" };
-        string[] palavrasAnimal = new string[] { "MACACO", "JACARE", "BALEIA", "URSO", "LEAO" };
-        string[] palavrasPersonagem = new string[] { "NARUTO", "GOKU", "LUFFY", "EREN" };
         string palavra;
+        Forca forca = new();
 
-        int dica = random.Next(0, 2);
-        if (dica == 0)
-        {
-            int palavras = random.Next(0, 5);
-            palavra = palavrasAnimal[palavras];
-        }
-        else
-        {
-            int palavras = random.Next(0, 4);
-            palavra = palavrasPersonagem[palavras];
+        forca.SorteaPalavra();
+        forca.OrganizaPalavra(forca.Palavra);
 
-        }
+        bool validou = false;
 
-        char[] palavraA = new char[palavra.Length];
-        for(int i = 0; i < palavraA.Length; i++)
-        {
-            palavraA[i] = '_';
-        }
-
-        int acerto = 0;
-
-        Forca forca = new Forca(palavra, palavraA);
-
+        int i = 0;
         do
         {
-            int i = 0;
-            Console.WriteLine("Dica: " + dicas[dica]);
+            Console.Clear();
+            Console.WriteLine("Dica: " + forca.Dica);
+            if (forca.LetrasDig != null)
+                Console.WriteLine(string.Join(" ", forca.LetrasDig));
             forca.Desenha();
-            Console.WriteLine(string.Join(" ", forca.PalavraArray));
+            Console.WriteLine(string.Join(" ", forca.LayoutPalavra));
             Console.WriteLine("Letra: ");
             string letraDigitada = Console.ReadLine();
             while( letraDigitada == null)
@@ -49,25 +30,11 @@ internal class Program
             }
             char letra = char.ToUpper(Convert.ToChar(letraDigitada));
 
-            forca.VerificaLetra(letra);
-            foreach (char c in forca.PalavraArray)
-            {
-                if (c == '_')
-                    break;
-                if (i == forca.PalavraArray.Length-1)
-                {
-                    Console.WriteLine(string.Join(" ", forca.PalavraArray));
-                    Console.WriteLine("Parabens!!!");
-                    acerto = 1;
-                }
-                i++;
-            }
-            if (forca.QtdErro == 6)
-            {
-                forca.Desenha();
-                Console.WriteLine("Que Pena. A palavra era: " + forca.Palavra);
-                break;
-            }
-        } while (acerto == 0);
+            forca.SalvaLetras(letra, i);
+            i++;
+            forca.VerificaLetra(letra, forca.Palavra);
+            validou = forca.Validacao();
+
+        } while (!validou);
     }
 }
