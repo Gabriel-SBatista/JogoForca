db21be25e2345cb22c8ea6507ae63dc9018f9322
Gabriel Batista <gabriel.dsbatista28@gmail.com>
2023-09-19 10:33:05 -0300

    primeiro commit

---
+++ Forca.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoForca
+{
+    internal class Forca
+    {
+        public string Palavra { get; private set; }
+        public int QtdErro { get; private set; }
+
+        public char[] PalavraArray { get; private set; }
+
+        public Forca(string palavra, char[] palavraArray)
+        {
+            Palavra = palavra;
+            PalavraArray = palavraArray;
+        }
+
+        public void VerificaLetra(char letra)
+        {
+            int i = 0;
+            int acerto = 0;
+            foreach (char c in this.Palavra)
+            {
+                if (c == letra)
+                {
+                    this.PalavraArray[i] = letra;
+                    acerto++;
+                }
+                if (i == this.Palavra.Length-1 && acerto == 0)
+                {
+               
+                    this.Erro();
+                }
+                i++;
+            }
+        }
+
+        public void Desenha()
+        {
+            switch (this.QtdErro)
+            {
+                case 1:
+                    Console.WriteLine("|");
+                    Console.WriteLine("|____O");
+                    break;
+                case 2:
+                    Console.WriteLine("|");
+                    Console.WriteLine("|____O");
+                    Console.WriteLine("     |");
+                    break;
+                case 3:
+                    Console.WriteLine("|");
+                    Console.WriteLine("|____O");
+                    Console.WriteLine("    /|");
+                    break;
+                case 4:
+                    Console.WriteLine("|");
+                    Console.WriteLine("|____O");
+                    Console.WriteLine("    /|\\");
+                    break;
+                case 5:
+                    Console.WriteLine("|");
+                    Console.WriteLine("|____O");
+                    Console.WriteLine("    /|\\");
+                    Console.WriteLine("    /");
+                    break;
+                case 6:
+                    Console.WriteLine("|");
+                    Console.WriteLine("|____O");
+                    Console.WriteLine("    /|\\");
+                    Console.WriteLine("    / \\");
+                    break;
+                default:
+                    Console.WriteLine("|");
+                    Console.WriteLine("|____");
+                    break;
+            }
+        }
+        
+        public void Erro()
+        {
+            this.QtdErro++;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using JogoForca;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random random = new Random();
+
+        string[] dicas = new string[] { "Animal", "Personagem de Anime" };
+        string[] palavrasAnimal = new string[] { "MACACO", "JACARE", "BALEIA", "URSO", "LEAO" };
+        string[] palavrasPersonagem = new string[] { "NARUTO", "GOKU", "LUFFY", "EREN" };
+        string palavra;
+
+        int dica = random.Next(0, 2);
+        if (dica == 0)
+        {
+            int palavras = random.Next(0, 5);
+            palavra = palavrasAnimal[palavras];
+        }
+        else
+        {
+            int palavras = random.Next(0, 4);
+            palavra = palavrasPersonagem[palavras];
+
+        }
+
+        char[] palavraA = new char[palavra.Length];
+        for(int i = 0; i < palavraA.Length; i++)
+        {
+            palavraA[i] = '_';
+        }
+
+        int acerto = 0;
+
+        Forca forca = new Forca(palavra, palavraA);
+
+        do
+        {
+            int i = 0;
+            Console.WriteLine("Dica: " + dicas[dica]);
+            forca.Desenha();
+            Console.WriteLine(string.Join(" ", forca.PalavraArray));
+            Console.WriteLine("Letra: ");
+            string letraDigitada = Console.ReadLine();
+            while( letraDigitada == null)
+            {
+                Console.WriteLine("Letra: ");
+                letraDigitada = Console.ReadLine();
+            }
+            char letra = char.ToUpper(Convert.ToChar(letraDigitada));
+
+            forca.VerificaLetra(letra);
+            foreach (char c in forca.PalavraArray)
+            {
+                if (c == '_')
+                    break;
+                if (i == forca.PalavraArray.Length-1)
+                {
+                    Console.WriteLine(string.Join(" ", forca.PalavraArray));
+                    Console.WriteLine("Parabens!!!");
+                    acerto = 1;
+                }
+                i++;
+            }
+            if (forca.QtdErro == 6)
+            {
+                forca.Desenha();
+                Console.WriteLine("Que Pena. A palavra era: " + forca.Palavra);
+                break;
+            }
+        } while (acerto == 0);
+    }
+}
